ca9b8a340536ab793152adabcfe8feaf60a59159
John Robinson <jorobinson@linkedin.com>
2016-07-21 13:40:08 -0700

    add iframe test

--- test/unit/util/iframe.spec.js
+++ test/unit/util/iframe.spec.js
@@ -1,1 +1,31 @@
-// TODO: write tests
+import Iframe from '../../../src/util/iframe';
+
+describe('Iframe', () => {
+  let iframe = new Iframe('#', { name: 'test' });
+  let iframeEl = iframe.iframe;
+
+  context('#constructor', () => {
+    it('should create a new iframe', () => {
+      expect(iframeEl).to.be.ok;
+    });
+
+    it('should set the src', () => {
+      expect(iframeEl.src).to.contain('#');
+    });
+
+    it('should add attributes', () => {
+      expect(iframeEl.width).to.equal('100%');
+      expect(iframeEl.name).to.equal('test');
+    });
+  });
+
+  context('#appendTo()', () => {
+    it('should append iframe to parent element', () => {
+      let iframeParent = document.createElement('div');
+
+      iframe.appendTo(iframeParent);
+
+      expect(iframeParent.firstChild).to.equal(iframeEl);
+    })
+  });
+});
